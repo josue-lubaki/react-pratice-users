d7d7cdc14c88867dce8ee87e82511c3b71bfe9ae
Josue Lubaki <josuelubaki@gmail.com>
2022-05-13 01:04:14 -0400

    retrieve user information with useState

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import styles from './AddUser.module.css';
 
 const AddUser = () => {
+	const [enteredUsername, setEnteredUsername] = useState('');
+	const [enteredAge, setEnteredAge] = useState('');
+
 	const addUserHandler = (event) => {
 		event.preventDefault();
+
+		const user = {
+			username: enteredUsername,
+			age: enteredAge,
+		};
+
+		console.log(user);
+	};
+
+	const usernameChangeHandler = (event) => {
+		setEnteredUsername(event.target.value);
+	};
+
+	const ageChangeHandler = (event) => {
+		setEnteredAge(event.target.value);
 	};
 
 	return (
 		<Card className={styles.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor='username'>Username</label>
-				<input type='text' id='username' />
+				<input
+					type='text'
+					id='username'
+					value={enteredUsername}
+					onChange={usernameChangeHandler}
+				/>
 				<label htmlFor='age'>Username</label>
-				<input type='number' id='age' />
+				<input
+					type='number'
+					id='age'
+					value={enteredAge}
+					onChange={ageChangeHandler}
+				/>
 				<Button type='submit'>Add user</Button>
 			</form>
 		</Card>
